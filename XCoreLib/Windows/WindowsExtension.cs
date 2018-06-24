53ed83ff0c33ff4aae41dd55df4e297f268578de
h1542462944 <t1542462994@outlook.com>
2018-06-24 21:13:17 +0800

    1.4.0.1
    加入Win32API

---
+++ XCoreLib/Windows/WindowsExtension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Drawing = System.Drawing;
+using System.Linq;
+using System.Text;
+using  System.Threading.Tasks;
+using Forms = System.Windows.Forms;
+using System.Windows;
+using System.Windows.Input;
+
+namespace XCore.Windows
+{
+    public static class WindowsTool
+    {
+        public static Point GetPositionFull()
+        {
+            Point abserbPoint = new Point(Forms.Cursor.Position.X, Forms.Cursor.Position.Y);
+            Size screenSize = new Size(Forms.Screen.PrimaryScreen.Bounds.Width, Forms.Screen.PrimaryScreen.Bounds.Height);
+            Vector dpi = new Vector(screenSize.Width / SystemParameters.PrimaryScreenWidth, screenSize.Height / SystemParameters.PrimaryScreenHeight);
+
+            
+
+            return new Point(abserbPoint.X / dpi.X, abserbPoint.Y / dpi.Y);
+            
+        }
+    }
+
+}
